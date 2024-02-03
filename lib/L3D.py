c2008e85ff1fc64f1170d040e0bad0650ed3dafd
sam <martinsa0182@gmail.com>
2024-02-03 22:54:11 +0000

    Added missing L3D file

---
+++ lib/L3D.py
@@ -0,0 +1,45 @@
+from lib.camera import Camera, CameraSettings
+from lib.led_identifier import LedFinder, LedResults
+import logging
+import cv2
+
+
+class L3D:
+
+    def __init__(self, device, exposure, threshold, width=-1, height=-1):
+        self.cam = Camera(device)
+
+        self.settings_backup = CameraSettings(self.cam)
+
+        if width != -1 or height != -1:
+            self.cam.set_resolution(width, height)
+
+        self.cam.set_autofocus(0, 0)
+        self.cam.set_exposure_mode(0)
+        self.cam.set_gain(0)
+        self.cam.set_exposure(exposure)
+
+        self.ditch_frames(20)
+
+        self.led_finder = LedFinder(threshold)
+
+    def __del__(self):
+        logging.info("reverting camera changes")
+        self.settings_backup.apply(self.cam)
+
+    def show_debug(self):
+
+        image = self.cam.read()
+        results = self.led_finder.find_led(image)
+        render_image = self.led_finder.draw_results(image, results)
+
+        cv2.imshow('Webcam Feed - Press ESC to close this window', render_image)
+        return cv2.waitKey(1) != 27
+
+    def ditch_frames(self, count=20):
+        for _ in range(count):
+            self.cam.read()
+
+    def find_led(self):
+        image = self.cam.read()
+        return self.led_finder.find_led(image)
